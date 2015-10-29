103dcc7d0f73bf37cbada542c6de42af289a4fff
Tobi Krebs <tobias.krebs@betterform.de>
2015-10-29 14:16:46 +0100

    - run selenium via ant

--- src/test/java/gov/state/history/TestCase1.java
+++ src/test/java/gov/state/history/TestCase1.java
@@ -1,15 +1,12 @@
 package gov.state.history;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.junit.*;
 import static org.junit.Assert.*;
-import static org.hamcrest.CoreMatchers.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
-public class TestCase2 {
+public class TestCase1 {
   private WebDriver driver;
   private String baseUrl;
   private boolean acceptNextAlert = true;
@@ -18,19 +15,22 @@ public class TestCase2 {
   @Before
   public void setUp() throws Exception {
     driver = new FirefoxDriver();
-    baseUrl = "http://localhost:8080/";
+    baseUrl = "http://localhost:8080";
     driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
   }
 
   @Test
-  public void testCase2() throws Exception {
+  public void testCase1() throws Exception {
     driver.get(baseUrl + "/exist/apps/hsg-shell/");
     assertEquals("history.state.gov 3.0 shell", driver.getTitle());
-    assertEquals("1750–1775", driver.findElement(By.xpath("(//a[contains(text(),'1750–1775')])[2]")).getText());
-    driver.findElement(By.xpath("(//a[contains(text(),'1750–1775')])[2]")).click();
-    assertEquals("Milestones: 1750–1775", driver.findElement(By.cssSelector("h1")).getText());
-    assertEquals("Rivalry in Europe, between the French and the British in particular, often influenced the course of events in their North American colonies.In an effort to increase their political and economic power, the British and the French competed to acquire the better share of the available land and control over the new trading opportunities the colonies presented.", driver.findElement(By.xpath("//div[@id='foreword']/p[2]")).getText());
-    assertEquals("Choose one 1750–1775 1776–1783 1784–1800 1801–1829 1830–1860 1861–1865 1866–1898 1899–1913 1914–1920 1921–1936 1937–1945 1945–1952 1953–1960 1961–1968 1969–1976 1977–1980 1981–1988 1989–1992 1993–2000 ... view all", driver.findElement(By.id("select-chapter")).getText());
+    driver.findElement(By.cssSelector("div.col-md-6 > ul > li > ul > li > a")).click();
+    assertEquals("Historical Documents", driver.findElement(By.cssSelector("h1")).getText());
+    assertEquals("Truman Administration \n 1945–1952", driver.findElement(By.cssSelector("td > a")).getText());
+    driver.findElement(By.cssSelector("td > a")).click();
+    assertEquals("Truman Administration", driver.findElement(By.cssSelector("h1.titleStmt1")).getText());
+    assertEquals("General: The United Nations, Volume I", driver.findElement(By.cssSelector("ol > li > a > span")).getText());
+    driver.findElement(By.cssSelector("ol > li > a > span")).click();
+    assertEquals("Not found", driver.findElement(By.cssSelector("h1")).getText());
   }
 
   @After
--- src/test/java/gov/state/history/Testsuite1.java
+++ src/test/java/gov/state/history/Testsuite1.java
@@ -1,18 +1,13 @@
 package gov.state.history;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
-
-public class Testsuite1 {
-
-  public static Test suite() {
-    TestSuite suite = new TestSuite();
-    suite.addTestSuite(TestCase1.class);
-    suite.addTestSuite(TestCase2.class);
-    return suite;
-  }
-
-  public static void main(String[] args) {
-    junit.textui.TestRunner.run(suite());
-  }
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+ 
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+  TestCase1.class,
+  TestCase2.class
+})
+ 
+public class Testsuite1 { 
 }
