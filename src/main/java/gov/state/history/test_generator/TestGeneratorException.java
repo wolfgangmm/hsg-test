fb0e76ff33b1338b31497c369ec5a7f99b6ccb90
Patryk Czarnik <patryk@patrykczarnik.net>
2015-11-13 10:31:00 +0100

    XSLT test generation run from a Java program, not from Ant (it crashed with an OutOfMem probbly due to a bug in Saxon)

---
+++ src/main/java/gov/state/history/test_generator/TestGeneratorException.java
@@ -0,0 +1,20 @@
+package gov.state.history.test_generator;
+
+public class TestGeneratorException extends Exception {
+
+	public TestGeneratorException() {
+	}
+
+	public TestGeneratorException(String message) {
+		super(message);
+	}
+
+	public TestGeneratorException(Throwable cause) {
+		super(cause);
+	}
+
+	public TestGeneratorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
